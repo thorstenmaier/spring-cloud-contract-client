eb4aa34dbce851c906cc5d524f8f70f8f2b426e3
Thorsten Maier <Thorsten.Maier@trivadis.com>
2022-02-03 07:58:12 +0100

    Initial project

---
+++ src/main/java/com/trivadis/client/SpringCloudContractClientApplication.java
@@ -0,0 +1,13 @@
+package com.trivadis.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudContractClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trivadis/client/SpringCloudContractClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trivadis.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudContractClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
